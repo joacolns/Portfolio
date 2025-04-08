193c5e8d55d6474ef0d2b28e2691bdbbbaddf8c9
njoaco <joaquinlucasnieto@gmail.com>
2025-04-08 15:34:29 -0300

    mail

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -15,10 +15,10 @@ function Intro() {
                 <a href="https://www.linkedin.com/in/njoaco" className="bg-gray-700 p-2 rounded-md shadow-md text-white">
                     <i className="fab fa-linkedin fa-lg" aria-hidden="true"></i>
                 </a>
-                <a href="https://github.com/njoaco" className="bg-gray-700 p-2 rounded-md shadow-md text-white">
+                <a href="https://github.com/joacolns" className="bg-gray-700 p-2 rounded-md shadow-md text-white">
                     <i className="fab fa-github fa-lg" aria-hidden="true"></i>
                 </a>
-                <a href="mailto:joaquinlucasnieto@gmail.com" className="bg-gray-700 p-2 rounded-md shadow-md text-white">
+                <a href="mailto:joaquinlucasnieto506@gmail.com" className="bg-gray-700 p-2 rounded-md shadow-md text-white">
                     <i className="fas fa-envelope fa-lg" aria-hidden="true"></i>
                 </a>
             </div>
