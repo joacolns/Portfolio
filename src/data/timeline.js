a8676ed582b8d2cd9897523ce9b480f8eb8f0b57
joacolns <joaquinlucasnieto@gmail.com>
2025-07-07 15:20:15 -0300

    timeline update

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,14 +3,14 @@ export default[
     {
         year: 'Hoy en día',
         title: 'Estudiante de Ingeniería en sistemas',
-        duration: 'Segundo Año',
+        duration: 'En: Segundo Año',
         details: 'Estoy estudiando la carrera de Ingeniería en Sistemas, un programa que me prepara para enfrentar y resolver desafíos tecnológicos complejos. A lo largo de mi formación, estoy adquiriendo habilidades en diversas áreas fundamentales como programación, análisis de sistemas, redes de computadoras, bases de datos y seguridad informática. Esta carrera no solo me capacita en el desarrollo de software, sino también en la integración y gestión de soluciones tecnológicas innovadoras que responden a las necesidades actuales de la industria y la sociedad. Estoy aprendiendo a aplicar un enfoque sistemático y analítico para optimizar los sistemas de información y contribuir al avance tecnológico.',
     },
     {
 
         year: 'Hoy en día',
         title: 'Desarrollador FullStack Junior',
-        duration: '2 Años',
+        duration: 'Desde hace: 2 Años',
         details: 'Soy desarrollador Full Stack Junior desde hace un año. Desde que comencé en este campo, me he dedicado a perfeccionar mis habilidades tanto en el desarrollo de backend como en frontend, utilizando tecnologías y herramientas modernas. Mi formación incluye haber completado un bootcamp en StreamBe, donde intensifiqué mi aprendizaje práctico en múltiples tecnologías clave. Aunque ya he logrado construir aplicaciones completas, desde la interfaz de usuario hasta la base de datos, estoy consciente de que el aprendizaje en tecnología nunca se detiene. Por eso, sigo capacitándome y actualizándome en las últimas tendencias y mejores prácticas del desarrollo web y software, para seguir creciendo profesionalmente y aportar cada vez más valor a los proyectos en los que trabajo.',
 
     },
@@ -18,7 +18,7 @@ export default[
 
         year: '2023',
         title: 'Técnico Electrónico',
-        duration: '1 Año',
+        duration: 'Duración: 7 Años',
         details: 'En 2023, completé mi educación secundaria técnica, obteniendo el título de Técnico Electrónico. A lo largo de mi formación, adquirí conocimientos sólidos en el diseño, construcción y mantenimiento de circuitos y dispositivos electrónicos. Esta base técnica me ha proporcionado las herramientas necesarias para analizar y resolver problemas complejos asociados a sistemas electrónicos y me ha preparado para enfrentar los desafíos tecnológicos actuales. Estoy entusiasmado por aplicar y expandir mis habilidades en el campo de la electrónica y la tecnología en general, buscando siempre integrar soluciones innovadoras en mis futuros proyectos y estudios avanzados.',
 
     },
@@ -26,7 +26,7 @@ export default[
 
         year: '2019',
         title: 'Práctica de desarrollo de videojuegos',
-        duration: '3 Años',
+        duration: 'Duración: 2 Años',
         details: 'En 2019, comencé a explorar el mundo de la creación de videojuegos, motivado por mi curiosidad sobre cómo se construyen. Empecé a practicar programación en C++, un lenguaje fundamental para el desarrollo de juegos por su eficiencia y control sobre los recursos del sistema. Además, me adentré en el uso de motores gráficos avanzados como Unity y Unreal Engine 4, que son herramientas esenciales para la creación de entornos visuales ricos y dinámicos, así como Clickteam Fusion 2.5 y Scratch, que me permitió experimentar con un enfoque más visual y menos codificado para el diseño de juegos.',
 
     }
